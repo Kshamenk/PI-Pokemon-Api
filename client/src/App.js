310c2b6f23a4eff7e401db0c351a170c7bf67b67
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-06-07 17:24:54 -0300

    comenzando el ruteo, todo vacio

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
     </div>
   );
 }
