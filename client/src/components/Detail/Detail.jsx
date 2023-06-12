7b97505d4974155fd5e026e58ba8dde881a079a5
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-06-12 20:28:05 -0300

    12062023  20:27

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
 import style from './Detail.module.css'
-// import { useDispatch , useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from "react-router-dom"
+import { getPokemonById } from '../../redux/actions';
+
 
 export default function Detail() {
-    return (
-      <div className={style.container}>
-        <h1>Detail</h1>
-      </div>
-    );
-  }
+  const { id } = useParams()
+  const dispatch = useDispatch()
+  const pokemonById = useSelector(state => state.pokemonById)
+  console.log(pokemonById)
+  useEffect(()=>{
+    dispatch(getPokemonById(id))
+  },[dispatch,id])
+
+
+  return (
+    <div className={style.container}>
+      <h2>{pokemonById.name}</h2>
+      <h3>{pokemonById.types}</h3>
+    </div>
+  );
+}
