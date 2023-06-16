90776e6d2bf1017ddb0602234511f6d3814d103d
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-06-16 13:12:25 -0300

    1312

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,27 +1,37 @@
-import { getPokeCreated } from '../../redux/actions'
+import { useState } from 'react';
+import { getAllPokemons, getPokeCreated } from '../../redux/actions'
 import style from './Filters.module.css'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 //created puede ser un parametro, if cF tirnr api ::: trae from api, else, traer los de la dbb   ()
 export default function Filters() {
-    const allPokemons = useSelector((state)=> state.allPokemons)
-    
+    //const allPokemons = useSelector((state)=> state.allPokemons)
+    //console.log(allPokemons)
+    const [filterType, setFilterType] = useState('');
+
     const dispatch = useDispatch()
 
+    const handleFilterType = (event) => {
+        setFilterType(event.target.value);
+        if (event.target.value === '') {
+          dispatch(getAllPokemons());
+        } else {
+          dispatch(getPokeCreated(event.target.value === 'dbb'));
+        }
+      };
+    // const handlerSelect = (event) => {
+    //     event.preventDefault()
+    //     dispatch(getPokeCreated(true))
+    // }
 
-    const handlerSelect = (event) => {
-        event.preventDefault()
-        dispatch(getPokeCreated(true))
-    }
 
-
-    return(
-        <div className={style.container} >
-            <label htmlFor="">Filtros</label>
-            <select name="" id="">
-                <option value="">Api</option>  
-                <option value="">Dbb</option>
-            </select>
-            <button onClick={handlerSelect}>Filtrar</button>
+    return (
+        <div className={style.container}>
+          <label htmlFor="filterType">Filtros</label>
+          <select name="filterType" id="filterType" value={filterType} onChange={handleFilterType}>
+            <option value="">Todos</option>
+            <option value="api">API</option>
+            <option value="dbb">DBB</option>
+          </select>
         </div>
-    )
-}
+      );
+    }
