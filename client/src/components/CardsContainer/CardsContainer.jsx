540f78413efeb0f4164d005f35e26e1febc8a35c
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-06-21 16:59:15 -0300

    165821062023

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -17,21 +17,27 @@ export default function CardsContainer() {
     const pokeFinal = [ ...pokeActuales]
     
     return (
-        <div className={style.container} >
-            <Paginate pokePerPage={pokePerPage}
+        <div className={style.container}>
+          <div className={style.paginate}>
+            <div className={style.buttons}>
+              <Paginate
+                pokePerPage={pokePerPage}
                 pokeActuales={allPokemons.length}
-                setCurrentPage={setCurrentPage} />
-            {
-                pokeFinal.map(poke => {
-                    return <Card
-                        key={poke.id}
-                        id={poke.id}
-                        name={poke.name}
-                        image={poke.image}
-                        types={poke.types}
-                    />
-                })
-            }
+                setCurrentPage={setCurrentPage}
+              />
+            </div>
+          </div>
+          {pokeFinal.map((poke) => {
+            return (
+              <Card
+                key={poke.id}
+                id={poke.id}
+                name={poke.name}
+                image={poke.image}
+                types={poke.types}
+              />
+            );
+          })}
         </div>
-    )
+      );
 }
