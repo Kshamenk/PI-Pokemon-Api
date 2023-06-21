c8d38037099e800a18344f28969a823a26a3dd4e
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-06-21 13:01:10 -0300

    12062023

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -3,30 +3,26 @@ import Card from '../Card/Card'
 import style from './CardsContainer.module.css'
 import { useState } from 'react'
 import Paginate from '../Paginate/Paginate'
-//import Paginate from '../Paginate/Paginate'
 
 export default function CardsContainer() {
 
     const allPokemons = useSelector(state => state.allPokemons)
-console.log(allPokemons.length)
+
     const [currentPage, setCurrentPage] = useState(1)  //pagina actual  (1)
     const pokePerPage = 12
-
+    
     const indexLastPoke = currentPage * pokePerPage  //(12)
     const indexFirstPoke = indexLastPoke - pokePerPage  //(0)
     const pokeActuales = allPokemons.slice(indexFirstPoke, indexLastPoke)
+    const pokeFinal = [ ...pokeActuales]
     
-
-
-
-
     return (
         <div className={style.container} >
             <Paginate pokePerPage={pokePerPage}
                 pokeActuales={allPokemons.length}
                 setCurrentPage={setCurrentPage} />
             {
-                pokeActuales.map(poke => {
+                pokeFinal.map(poke => {
                     return <Card
                         key={poke.id}
                         id={poke.id}
--- client/src/components/Paginate/Paginate.jsx
+++ client/src/components/Paginate/Paginate.jsx
@@ -2,24 +2,24 @@ import style from './Paginate.module.css'
 
 export default function Paginate({ pokePerPage, pokeActuales, setCurrentPage }) {
 
-    
+
     const totalPages = Math.ceil(pokeActuales / pokePerPage);
-    
+
     const pageNumber = []
 
     for (let i = 1; i <= totalPages; i++) {
-       pageNumber.push(i) 
-        
+        pageNumber.push(i)
+
     }
-    
+
 
     return (
         <div className={style.container}>
             {
-                pageNumber && 
-                pageNumber.map((num)=> (
-                    
-                    <button key={num} onClick={()=>setCurrentPage(num)} >
+                pageNumber &&
+                pageNumber.map((num) => (
+
+                    <button key={num} onClick={() => setCurrentPage(num)} >
                         {num}
                     </button>
                 ))
