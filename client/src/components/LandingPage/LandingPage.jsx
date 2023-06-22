e32d6aa49f734fb9b9e773dd756693befa2013d9
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-06-22 17:26:20 -0300

    17262023

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,16 +1,3 @@
-// import style from './LandingPage.module.css'
-// import { Link } from 'react-router-dom'
-
-// export default function LandingPage() {
-//     return (
-//         <div className={style.container}>
-//             <h1>LandingPage</h1>
-//             <Link to='/home'>
-//                 <h2>Home</h2>
-//             </Link>
-//         </div>
-//     )
-// }
 
 
 
