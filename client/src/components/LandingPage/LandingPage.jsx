3288f8539f57f84d5f49e2bcf5c5974e8841a81d
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-06-22 12:47:25 -0300

    mas estilos.....

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,31 @@
-import style from './LandingPage.module.css'
-import { Link } from 'react-router-dom'
+// import style from './LandingPage.module.css'
+// import { Link } from 'react-router-dom'
+
+// export default function LandingPage() {
+//     return (
+//         <div className={style.container}>
+//             <h1>LandingPage</h1>
+//             <Link to='/home'>
+//                 <h2>Home</h2>
+//             </Link>
+//         </div>
+//     )
+// }
+
+
+
+import pokemonImage from '../../Img/pokemon.jpg';
+import React from 'react';
+import style from './LandingPage.module.css';
+import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
-    return (
-        <div className={style.container}>
-            <h1>LandingPage</h1>
-            <Link to='/home'>
-                <h2>Home</h2>
-            </Link>
-        </div>
-    )
+  return (
+    <div className={style.container}>
+      <img src={pokemonImage} alt="Pokemon" className={style.image} />
+      <Link to="/home" className={style.button}>
+        Vamos!
+      </Link>
+    </div>
+  );
 }
